5d11d83bcba06140dc7e1e5c045e99deee3d21a4
Nathan Cahill <nathan@nathancahill.com>
2018-02-28 15:56:45 -0700

    enable dump

--- user.js
+++ user.js
@@ -3,3 +3,4 @@ user_pref("devtools.debugger.force-local", false);
 user_pref("devtools.debugger.remote-enabled", true);
 user_pref("devtools.chrome.enabled", true);
 user_pref("devtools.debugger.prompt-connection", false);
+user_pref("browser.dom.window.dump.enabled", true);
